3f37fc4ad84009d2ff6fbafa5e16d17d1ff6309b
dejanastevanovic <dejana.stevanovic@flaschenpost.ch>
2024-04-21 01:06:27 +0200

    add user-service

---
+++ src/test/java/stevanovic/dejana/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package stevanovic.dejana.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
